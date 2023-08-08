8cbdfecd83c9405fbe71a009fd68297d53c459b9
Leon-101 <Leon_101@foxmail.com>
2023-08-08 11:46:15 +0800

    Create website frontend using Vue and configure router

---
+++ website/src/components/UnderConstruction.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="under-construction">
+		<p>功能开发中……</p>
+		<p>敬请期待！</p>
+	</div>
+</template>
+
+<style>
+.under-construction {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	min-height: 100vh;
+	font-size: 18px;
+	font-weight: bold;
+}
+</style>
+  
---
+++ website/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import UnderConstruction from '../components/UnderConstruction.vue';
+</script>
+
+<template>
+  <main>
+    <UnderConstruction />
+  </main>
+</template>
