d908cb9cceae1cad1728dcb57a0b203106da2aad
shengrihui <1120058252@qq.com>
2023-08-11 12:14:24 +0800

    scripts/

--- web_django/app01/views.py
+++ web_django/app01/views.py
@@ -12,7 +12,25 @@ def login(request):
 
 
 def scripts_list(request):
-    pass
+    sort_by = request.GET.get("sort_by", "create_time")
+    order = "-" if request.GET.get("order", "asc") == "desc" else ""  # 如果是desc是降序，默认升序
+    order_by = order + sort_by
+    limit = int(request.GET.get("limit", "10"))
+    offset = int(request.GET.get("offset", "1"))
+
+    queryset = Script.objects.all().order_by(order_by)[offset - 1:offset - 1 + limit]
+    resp_data = []
+    for obj in queryset:
+        obj_data = {"id": obj.id,
+                    "title": obj.title,
+                    "description": obj.description,
+                    "author": obj.author.username,
+                    "stars": obj.stars,
+                    "create_time": obj.create_time
+                    }
+        resp_data.append(obj_data)
+    resp = {"data": resp_data}
+    return JsonResponse(resp)
 
 
 def scripts_detail(request):
@@ -23,7 +41,7 @@ def scripts_detail(request):
             "description": row.description,
             "stars": row.stars,
             "create_time": row.create_time,
-            "script_url":row.script_path,
+            "script_url": row.script_path,
             }
 
     return JsonResponse(data)
