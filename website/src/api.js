2d4a1c2e0758699de5e8d03ec1f0f925e1eebae8
Leon-101 <Leon_101@foxmail.com>
2023-08-16 21:13:24 +0800

    Add interceptors of api request

--- website/src/api.js
+++ website/src/api.js
@@ -1,9 +1,20 @@
 import axios from 'axios'
 
-export default axios.create({
+const api = axios.create({
 	baseURL: '/api',
 	timeout: 30000,
 	headers: {
 		'Content-Type': 'application/json',
+		// 'Content-Type': 'application/x-www-form-urlencoded',
 	}
 });
+
+// 添加响应拦截器
+api.interceptors.response.use(function (response) {
+	return response;
+}, function (error) {
+	console.error(error);
+	return Promise.reject(error);
+});
+
+export default api;
