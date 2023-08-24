a0c0dd0d287aa4a1299ce2ff26cdc762f64be541
Leon-101 <Leon_101@foxmail.com>
2023-08-24 22:24:24 +0800

    Feat: document title follows page changed

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -10,43 +10,67 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: '首页',
+      },
     },
     {
       path: '/scripts',
       name: 'scripts',
       component: () => import('../views/ScriptsView.vue'),
+      meta: {
+        title: '脚本库',
+      },
     },
     {
       path: '/needs',
       name: 'needs',
-      component: () => import('../views/NeedsView.vue')
+      component: () => import('../views/NeedsView.vue'),
+      meta: {
+        title: '需求趋',
+      },
     },
     {
       path: '/user_center',
       name: 'user_center',
       component: () => import('../views/UserCenterView.vue'),
-      meta: { requiresAuth: true },
+      meta: {
+        title: '个人中心',
+        requiresAuth: true,
+      },
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: '关于我们',
+      },
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('../views/RegisterView.vue')
+      component: () => import('../views/RegisterView.vue'),
+      meta: {
+        title: '注册',
+      },
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LoginView.vue'),
+      meta: {
+        title: '登录',
+      },
     },
     {
       path: '/reset_password',
       name: 'reset_password',
-      component: () => import('../views/ResetPasswordView.vue')
+      component: () => import('../views/ResetPasswordView.vue'),
+      meta: {
+        title: '忘记密码',
+      },
     },
     {
       path: '/script_details/:script_id',
@@ -57,7 +81,10 @@ const router = createRouter({
       path: '/script_upload',
       name: 'script_upload',
       component: () => import('../views/ScriptUploadView.vue'),
-      meta: { requiresAuth: true },
+      meta: {
+        title: '上传脚本',
+        requiresAuth: true,
+      },
     },
     {
       path: '/need_details/:script_id',
@@ -68,7 +95,10 @@ const router = createRouter({
       path: '/need_upload',
       name: 'need_upload',
       component: () => import('../views/NeedUploadView.vue'),
-      meta: { requiresAuth: true },
+      meta: {
+        title: '发布需求',
+        requiresAuth: true,
+      },
     },
   ]
 })
@@ -88,5 +118,12 @@ router.beforeEach((to, from) => {
     }
   }
 })
+
+// 路由切换后更改页面标题
+router.afterEach((to, from) => {
+  const baseTitle = "无障碍优化平台";
+  const pageTitle = to.meta.title || "";
+  document.title = pageTitle ? `${pageTitle} - ${baseTitle}` : baseTitle;
+});
 
 export default router
